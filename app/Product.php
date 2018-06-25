5cf25b6f758c24492cec14340eaa9e440715ef1c
Apoorva Chitre <apoorva09@gmail.com>
2018-06-25 16:15:55 -0400

    Relationships created

--- app/Product.php
+++ app/Product.php
@@ -4,7 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Review;
+
 class Product extends Model
 {
-    //
+    public function reviews() {
+
+    	return $this->hasMany(Review::class);
+    }
 }
