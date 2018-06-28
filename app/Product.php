f78319bb2c982c3825eb150405c9ab54fe04224f
Apoorva Chitre <apoorva09@gmail.com>
2018-06-28 13:14:59 -0400

    Product Create and Update

--- app/Product.php
+++ app/Product.php
@@ -8,6 +8,12 @@ use App\Review;
 
 class Product extends Model
 {
+    
+	protected $fillable = [
+
+		'name', 'detail', 'price', 'stock', 'discount'
+	];
+
     public function reviews() {
 
     	return $this->hasMany(Review::class);
